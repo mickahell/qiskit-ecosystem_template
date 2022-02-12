9b95a9dc0bb784810a9e50cdeb151b364453b312
Michaël <mika89700@gmail.com>
2022-02-12 10:34:51 +0100

    Update random.py

--- src/random.py
+++ src/random.py
@@ -1,6 +1,7 @@
 """Docstring."""
 import math
 from typing import Union
+from qiskit import Aer, QuantumCircuit, execute, IBMQ
 
 
 class Random:
@@ -9,11 +10,28 @@ class Random:
     def __init__(self):
         """Demo random."""
         self.pow = 2
+        self.qasm = Aer.get_backend('qasm_simulator')
 
     def run(self, number: Union[int, float]) -> Union[int, float]:
         """Run method."""
+        nb_qubits = number
         print("Generate random number")
-        return math.pow(number, self.pow)
+        
+        circ = QuantumRegister(nb_qubits, 'the number')
+        c = ClassicalRegister(nb_qubits, 'measurement')
+        qc = QuantumCircuit(circ, c)
+        
+        for i in range(0, nb_qubits):
+            qc.x(i)
+        qc.measure(circ, c)
+        
+        job = execute(qc, self.qasm, shots=1, memory=True)
+        
+        result_sim = job.result()
+        memory = result_sim.get_memory()
+        result = int(memory[0], 2) + 1
+    
+        return result
 
     def __repr__(self):
-        return f"Random(pow: {self.pow})"
+        return f"Random(circuit of: {self.pow})"
